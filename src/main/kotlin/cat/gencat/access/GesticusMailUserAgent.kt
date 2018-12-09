c7154bdf8d15ceef5834cbdf42729504cc15ac9f
Pep Mendez <jmendez1@xtec.cat>
2018-12-09 10:01:34 +0100

    GesticusMailUserAgent docuented

--- src/main/kotlin/cat/gencat/access/GesticusMailUserAgent.kt
+++ src/main/kotlin/cat/gencat/access/GesticusMailUserAgent.kt
@@ -12,21 +12,56 @@ const val USER_PASSWORD = "8lMEuDlsEyZUuskwrSeecVKF/1bUDcEk"
 
 const val SECRET_PASSWORD = "secret"
 
-const val PORT_SSL = 465
-const val PORT_TLS = 587
+const val PORT_OLD_SSL = 465
+const val PORT_TLS_SUBMISSION = 587
 
-class GesticusEmailClient {
+/*
+*
+* First ever email 1971
+*
+* SSL and TLS both provide a way to encrypt a communication channel between two computers.
+*
+* TLS is the successor of SSL and both terms are used interchangeably in general.
+*
+* STARTTLS is a way to tacke an existing insecure connections and upgrade it to a secure
+* connection using SSL/TLS.
+*
+* Version numbering is inconsistent between SSL and TLS. When TLS took over from SSL as the
+* preferred protocol name, it began a new version number, and also began using sub-versions.
+*
+* SSL v2, SSL v3, TLS v1.0, TLS v1.1, TLS v1.2, TLS v.1.3
+*
+* SMTP (Simple Mail Protocol) uses port 23 and was originally designed for transfer, not submission.
+*
+* So yet another port (587) was defined for message submission.
+*
+* In most cases, systems that offer message submission over port 587 require clients to use STARTTLS
+* to upgrade the connections and also require a username and password to authenticate.
+*
+* */
+
+class GesticusMailUserAgent {
 
     companion object {
 
         private val props = Properties().apply {
 
             put("mail.debug", "true")
+            // SMTP
             put("mail.transport.protocol", "smtp")
+            // Free GMAIL smtp server
             put("mail.smtp.host", "smtp.gmail.com")
-            put("mail.smtp.port", PORT_TLS)
+            // Free Hotmail stmp server
+            // put("mail.smtp.host", "smtp.live.com")
+            put("mail.smtp.port", PORT_TLS_SUBMISSION)
             put("mail.smtp.auth", "true")
+            // STARTTLS
             put("mail.smtp.starttls.enable", "true")
+            // SSL
+//            put("mail.smtp.socketFactory.port", "465")
+//            put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory")
+
+
         }
 
         private val authenticator = object : Authenticator() {
@@ -45,19 +80,20 @@ class GesticusEmailClient {
             vararg addresses: String
         ): Unit {
 
-
             val session = Session.getInstance(props, authenticator)
 
             val message: MimeMessage = MimeMessage(session)
+            message.sentDate = Date()
             message.setFrom(InternetAddress(USER_NAME))
             message.setRecipients(
                 Message.RecipientType.TO,
-                InternetAddress.parse(USER_NAME)
+                InternetAddress.parse(USER_NAME, true)
             )
+
             for (address in addresses) {
                 message.setRecipients(
                     Message.RecipientType.CC,
-                    InternetAddress.parse(address)
+                    InternetAddress.parse(address, true)
                 )
             }
             message.setSubject(subject);
