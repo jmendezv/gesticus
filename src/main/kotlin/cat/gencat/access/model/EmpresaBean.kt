020614d44fc980d6c9deb14d855aa181cd205a18
Pep Mendez <jmendez1@xtec.cat>
2019-04-30 09:10:23 +0200

    Simple comment added to EmpresaBean

--- src/main/kotlin/cat/gencat/access/model/EmpresaBean.kt
+++ src/main/kotlin/cat/gencat/access/model/EmpresaBean.kt
@@ -5,6 +5,7 @@ import javafx.beans.property.SimpleObjectProperty
 import javafx.beans.property.SimpleStringProperty
 import tornadofx.*
 
+/* This object is used in ImpresesView */
 class EmpresaBean(
     id: Int,
     nif: String,
