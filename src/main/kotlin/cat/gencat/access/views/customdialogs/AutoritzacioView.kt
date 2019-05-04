229ebef501ff4a457b00f8d536f92d76e035da9a
Pep Mendez <jmendez1@xtec.cat>
2019-05-04 14:35:27 +0200

    TODO.md file updated

--- src/main/kotlin/cat/gencat/access/views/customdialogs/AutoritzacioView.kt
+++ src/main/kotlin/cat/gencat/access/views/customdialogs/AutoritzacioView.kt
@@ -2,11 +2,7 @@ package cat.gencat.access.views.customdialogs
 
 import tornadofx.*
 
-/*
-*
-* NOTE: USE A WIZARD
-*
-* */
+/* NOTE: USE A WIZARD */
 class AutoritzacioView : Wizard("Autorització d'una ordre de serveis",
     "Per al personal al servei de l'Administració i de la despesa corresponent.") {
 
