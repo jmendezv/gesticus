bc42b764b196198f296b61f065056c3c85bcdb42
Pep Mendez <jmendez1@xtec.cat>
2019-06-11 08:39:18 +0200

    Added CSVBean data class

---
+++ src/main/kotlin/cat/gencat/access/model/CSVBean.kt
@@ -0,0 +1,12 @@
+package cat.gencat.access.model
+
+import java.util.*
+
+data class CSVBean(
+    val codyAny: String,
+    val codiActivitat: String,
+    val codiPersona: String,
+    val nomActivitat: String,
+    val numHoresPrevistes: Int,
+    val dataInici: Date,
+    val dataFinal: Date)
